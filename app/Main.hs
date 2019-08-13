1e46017408ed96bdc9fd176d1fb333fe3fa3bd76
jlkiri <rensakamoto7@gmail.com>
2019-08-13 20:24:01 +0900

    Try scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import HandCheck.Check
+import Web.Scotty
+import Network.Wai.Middleware.Static
+
+showLandingPage :: ActionM ()
+showLandingPage = do
+  setHeader "Content-Type" "text/html"
+
+routes :: ScottyM ()
+routes = do
+  middleware $ staticPolicy $ addBase "/static"
 
 main :: IO ()
-main = print isWinning
+main = do
+  putStrLn "Starting Server..."
+  scotty 3000 routes
