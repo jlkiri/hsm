5291e60ba8b3a95ac1bada67ccf8be0923272612
jlkiri <rensakamoto7@gmail.com>
2019-08-13 20:25:20 +0900

    Remove scotty

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Web.Scotty
-import Network.Wai.Middleware.Static
-
-showLandingPage :: ActionM ()
-showLandingPage = do
-  setHeader "Content-Type" "text/html"
-
-routes :: ScottyM ()
-routes = do
-  middleware $ staticPolicy $ addBase "/static"
-
 main :: IO ()
-main = do
-  putStrLn "Starting Server..."
-  scotty 3000 routes
+main = putStrLn "Handcheck"
